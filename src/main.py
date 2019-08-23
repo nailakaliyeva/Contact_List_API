93a52cce4cd8bf8b81dfb2aa887dd4ffb7f2f535
Naila <nailakaliyeva@gmail.com>
2019-08-23 18:07:13 +0000

    created a model and validated it and add methods

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,6 +36,45 @@ def handle_person():
     }
 
     return jsonify(response_body), 200
+
+@app.route("/contact", methods=['POST', 'GET'])
+def handle_contact():
+    if  request.method == "POST":
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'full_name' not in body:
+            raise APIException('You need to specify the full name', status_code=400)
+        if 'email' not in body:
+            raise APIException('You need to specify the email', status_code=400)
+        contact1 = Person(full_name=body['full_name'], email=body['email'], address=body["address"], phone=body["phone"])
+        db.session.add(contact1)
+        db.session.commit()
+        return "ok", 200
+    # GET request
+    if request.method == 'GET':
+        all_people = Person.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return jsonify(all_people), 200
+    return "Invalid Method", 404
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    full_name = db.Column(db.String(50), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    # def __repr__(self):
+    #     return '<Person %r>' % self.full_name
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "address": self.address,
+            "email": self.email,
+            "phone": self.phone,
+            "full_name": self.full_name,
+            "id": self.id
+        }
